79a5d097bedc32383e1f3a3298e3ed8d9d2ac2f6
Toni <Toni@DESKTOP-C45V5I7>
2018-05-27 20:32:26 +0200

    Nov Zacetek-Spring Boot

---
+++ PraktikumDogodki/src/main/java/com/praktikum/dogodki/PraktikumDogodkiApplication.java
@@ -0,0 +1,12 @@
+package com.praktikum.dogodki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraktikumDogodkiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraktikumDogodkiApplication.class, args);
+	}
+}
---
+++ PraktikumDogodki/src/test/java/com/praktikum/dogodki/PraktikumDogodkiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.praktikum.dogodki;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PraktikumDogodkiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
