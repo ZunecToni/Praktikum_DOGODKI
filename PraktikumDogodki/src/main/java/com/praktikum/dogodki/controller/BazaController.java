c47b6452ca17ebac16da88115d787e67894a7a04
Toni <Toni@DESKTOP-C45V5I7>
2018-05-29 17:37:58 +0200

    q

--- PraktikumDogodki/src/main/java/com/praktikum/dogodki/controller/BazaController.java
+++ PraktikumDogodki/src/main/java/com/praktikum/dogodki/controller/BazaController.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class BazaController {
 	
-	@RequestMapping(value = {cghdfhddgh});
+	@RequestMapping(value = {cghABRAh});
 
 }
