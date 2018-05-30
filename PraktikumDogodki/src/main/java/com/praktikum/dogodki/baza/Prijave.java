87db62387b820b05cb5e63b465dd11bf8d072156
Master <Master@164.8.161.213>
2018-05-30 10:34:07 +0200

    a gre?

---
+++ PraktikumDogodki/src/main/java/com/praktikum/dogodki/baza/Prijave.java
@@ -0,0 +1,5 @@
+package com.praktikum.dogodki.baza;
+
+public class Prijave {
+
+}
---
+++ PraktikumDogodki/src/main/java/com/praktikum/dogodki/entities/Prijava.java
@@ -0,0 +1,74 @@
+
+ 
+ package com.praktikum.dogodki.entities;
+
+public class Prijava {
+	String ime;
+	String priimek;
+	String email;
+	int Ocena;
+	String prijava;
+	String status;
+	
+	
+
+	public Prijava (String ime, String priimek, String email ) {
+		this.ime = ime;
+		this.priimek = priimek;
+		this.email = email;
+		
+	}
+	public Prijava (String ime, String priimek, String email,int Ocena,String prijava,String status ) {
+		this.ime = ime;
+		this.priimek = priimek;
+		this.email = email;
+		this.Ocena = Ocena;
+		this.prijava = prijava;
+		this.status=status;
+		
+	}
+
+
+	public String getIme() {
+		return ime;
+	}
+
+
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+
+
+	public String getPriimek() {
+		return priimek;
+	}
+
+
+	public void setPriimek(String priimek) {
+		this.priimek = priimek;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+
+	
+	
+	public String toString() {
+		return "Uporabnik " + ime+ " " + priimek +"/n"+
+		"Email : "+ email + "/n";
+		
+	}
+	
+	
+	
+
+}
