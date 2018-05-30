8d23cdab24d0aeef7bcc22f7feb8a84768571f15
Toni <Toni@164.8.163.65>
2018-05-30 10:30:05 +0200

    .

--- PraktikumDogodki/src/main/java/com/praktikum/dogodki/controller/DogodekDao.java
+++ PraktikumDogodki/src/main/java/com/praktikum/dogodki/controller/DogodekDao.java
@@ -1,10 +1,15 @@
 package com.praktikum.dogodki.controller;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+
+import com.praktikum.dogodki.entities.Dogodek;
 
 @Component
 public class DogodekDao {
@@ -17,5 +22,30 @@ public class DogodekDao {
 		
 		
 	} 
+	
+	public List<Dogodek> getVseDogodke(){
+		String sql = "SELECT * FROM DOGODEK";
+		List<Dogodek> ret = new ArrayList<Dogodek>();
+		List<Map<String,Object>> rows = jdbcTemplate.queryForList(sql);
+		for (Map row : rows) {
+			
+			
+			 String naziv= (String)row.get("naziv");
+			 String opis= (String)row.get("opis");
+			 String lokacija= (String)row.get("lokacija");
+			 Date zacetek= (Date)row.get("zacetek");
+			 Date trajanje= (Date)row.get("trajanje");
+			 Date konecPrijav= (Date)row.get("konecPrijav");
+			 Date konecOdjav= (Date)row.get("konecOdjav");
+			 int minOseb= (int)row.get("minOseb");
+			 int maxOseb= (int)row.get("maxOseb");
+			 String status= (String)row.get("status");
+			 boolean potrebnoPovabilo = (boolean)row.get("potrebnoPovabilo");
+			
+
+			ret.add(new Dogodek(naziv, opis, lokacija, zacetek, trajanje, konecPrijav, konecOdjav, minOseb, maxOseb, status, potrebnoPovabilo));
+		}
+		return ret;
+	}
 
 }
