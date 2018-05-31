c67d2e62e6bb352359f4327b7532a7db44a2ee9b
Toni <Toni@GlaDOS>
2018-06-01 01:08:51 +0200

    ne dela!

--- PraktikumDogodki/src/main/java/com/praktikum/dogodki/baza/BazaMySQL.java
+++ PraktikumDogodki/src/main/java/com/praktikum/dogodki/baza/BazaMySQL.java
@@ -1,31 +1,31 @@
-//package com.praktikum.dogodki.baza;
-//
-//
-//
-//	import java.sql.Connection;
-//	import java.sql.DriverManager;
-//
-//	public class BazaMySQL {
-//		
-//		public static void main (String [] args) throws Exception {
-//			getConnection();
-//		}
-//		
-//		
-//		public static Connection getConnection() throws Exception{
-//			
-//			try {
-//				String driver = "com.mysql.jdbc.Driver";
-//				String url = "jdbc:127.0.0.1:3306/baza";
-//				String username= "ZunecToni";
-//				String password = "dogodki2018";
-//				Class.forName(driver);
-//				
-//				Connection conn = DriverManager.getConnection(url,username,password);
-//				System.out.println("Connected");
-//				return conn;
-//			}catch(Exception e) {System.out.println(e);}
-//			
-//			return null;
-//		}
-//	}
+package com.praktikum.dogodki.baza;
+
+
+
+	import java.sql.Connection;
+	import java.sql.DriverManager;
+
+	public class BazaMySQL {
+		
+		public static void main (String [] args) throws Exception {
+			getConnection();
+		}
+		
+		
+		public static Connection getConnection() throws Exception{
+			
+			try {
+				String driver = "com.mysql.jdbc.Driver";
+				String url = "jdbc:127.0.0.1:3306/baza";
+				String username= "ZunecToni";
+				String password = "dogodki2018";
+				Class.forName(driver);
+				
+				Connection conn = DriverManager.getConnection(url,username,password);
+				System.out.println("Connected");
+				return conn;
+			}catch(Exception e) {System.out.println(e);}
+			
+			return null;
+		}
+	}
---
+++ PraktikumDogodki/src/main/java/com/praktikum/dogodki/baza/DogodekRepository.java
@@ -0,0 +1,15 @@
+package com.praktikum.dogodki.baza;
+
+import org.springframework.data.repository.CrudRepository;
+
+public class DogodekRepository {
+
+	
+		public interface UserRepository extends CrudRepository<Dogodki, Long> {
+
+		}
+
+		
+
+	
+}
