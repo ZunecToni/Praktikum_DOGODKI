0035743df7f4daf3f2c25ae687f599f08d71f448
Master <Master@164.8.160.127>
2018-06-05 14:33:17 +0200

    a

--- PraktikumDogodki/src/main/java/com/praktikum/dogodki/PraktikumDogodkiApplication.java
+++ PraktikumDogodki/src/main/java/com/praktikum/dogodki/PraktikumDogodkiApplication.java
@@ -1,12 +1,36 @@
 package com.praktikum.dogodki;
 
+import javax.sql.DataSource;
+ 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+ 
+import com.praktikum.dogodki.baza.DogodekRepository;
+ 
 @SpringBootApplication
+@EnableJpaRepositories("com.praktikum.dogodki.baza")
+@EntityScan("com.praktikum.dogodki.entities")
 public class PraktikumDogodkiApplication {
+	 @Autowired
+	    DataSource dataSource;
+	 
+	    @Autowired
+	    DogodekRepository DogodekRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PraktikumDogodkiApplication.class, args);
 	}
+	
+
+//    public void run(String[] args) throws Exception {
+//        System.out.println("Our DataSource is = " + dataSource);
+//        Iterable<com.praktikum.dogodki.baza.Dogodki> systemlist = DogodekRepository.findAll();
+//        for(com.praktikum.dogodki.baza.Dogodki systemmodel:systemlist){
+//            System.out.println("Here is a system: " + systemmodel.toString());
+//        }
+//    }
 }
