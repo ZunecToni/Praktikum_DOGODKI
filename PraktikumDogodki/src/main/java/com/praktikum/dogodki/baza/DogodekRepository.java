d5f383d52f069547bde8690ffcc2504b3589c86d
Toni <Toni@164.8.162.175>
2018-06-06 10:29:50 +0200

    1

--- PraktikumDogodki/src/main/java/com/praktikum/dogodki/baza/DogodekRepository.java
+++ PraktikumDogodki/src/main/java/com/praktikum/dogodki/baza/DogodekRepository.java
@@ -1,26 +1,26 @@
-package com.praktikum.dogodki.baza;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import com.praktikum.dogodki.baza.DogodekRepository;
-
-@Repository
-public interface DogodekRepository extends CrudRepository<Dogodki,Long> {
-	
-
-}
 //package com.praktikum.dogodki.baza;
 //
 //import org.springframework.data.repository.CrudRepository;
+//import org.springframework.stereotype.Repository;
+//import com.praktikum.dogodki.baza.DogodekRepository;
 //
-//public class DogodekRepository  {
-//
+//@Repository
+//public interface DogodekRepository extends CrudRepository<Dogodki,Long> {
 //	
-//		public interface UserRepository extends CrudRepository<Dogodki, Long> {
 //
-//		}
-//
-//		
-//
-//	
 //}
+////package com.praktikum.dogodki.baza;
+////
+////import org.springframework.data.repository.CrudRepository;
+////
+////public class DogodekRepository  {
+////
+////	
+////		public interface UserRepository extends CrudRepository<Dogodki, Long> {
+////
+////		}
+////
+////		
+////
+////	
+////}
--- PraktikumDogodki/src/main/java/com/praktikum/dogodki/baza/Dogodki.java
+++ PraktikumDogodki/src/main/java/com/praktikum/dogodki/baza/Dogodki.java
@@ -1,153 +1,153 @@
-/*
- * 
- * 
- * 
- * 
- * test za MySQL bazo
- * 
- * https://spring.io/guides/gs/accessing-data-mysql/
- * 
- * 
- * */
-
-package com.praktikum.dogodki.baza;
-
-import java.sql.Date;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-@Entity
-public class Dogodki {
-
-		
-
-		//naziv, opis, lokacija, zacetek, trajanje, konecPrijav, konecOdjav, minOseb, maxOseb, status, potrebnoPovabilo
-		//LocalDateTime.now()
-		@Id
-		@GeneratedValue(strategy=GenerationType.AUTO)
-		private String naziv;	
-		private String opis;
-		private String lokacija;
-		private Date zacetek;
-		private Date trajanje;
-		private Date konecPrijav;
-		private Date konecOdjav;
-		private int minOseb;
-		private int maxOseb;
-		private String status;
-		private boolean potrebnoPovabilo;
+///*
+// * 
+// * 
+// * 
+// * 
+// * test za MySQL bazo
+// * 
+// * https://spring.io/guides/gs/accessing-data-mysql/
+// * 
+// * 
+// * */
+//
+//package com.praktikum.dogodki.baza;
+//
+//import java.sql.Date;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//
+//@Entity
+//public class Dogodki {
+//
 //		
-//		public Dogodek(String naziv, String opis, String lokacija, Date zacetek, Date trajanje, Date konecPrijav,
-//				Date konecOdjav, int minOseb, int maxOseb, String status, boolean potrebnoPovabilo) {
-//			super();
+//
+//		//naziv, opis, lokacija, zacetek, trajanje, konecPrijav, konecOdjav, minOseb, maxOseb, status, potrebnoPovabilo
+//		//LocalDateTime.now()
+//		@Id
+//		@GeneratedValue(strategy=GenerationType.AUTO)
+//		private String naziv;	
+//		private String opis;
+//		private String lokacija;
+//		private Date zacetek;
+//		private Date trajanje;
+//		private Date konecPrijav;
+//		private Date konecOdjav;
+//		private int minOseb;
+//		private int maxOseb;
+//		private String status;
+//		private boolean potrebnoPovabilo;
+////		
+////		public Dogodek(String naziv, String opis, String lokacija, Date zacetek, Date trajanje, Date konecPrijav,
+////				Date konecOdjav, int minOseb, int maxOseb, String status, boolean potrebnoPovabilo) {
+////			super();
+////			this.naziv = naziv;
+////			this.opis = opis;
+////			this.lokacija = lokacija;
+////			this.zacetek = zacetek;
+////			this.trajanje = trajanje;
+////			this.konecPrijav = konecPrijav;
+////			this.konecOdjav = konecOdjav;
+////			this.minOseb = minOseb;
+////			this.maxOseb = maxOseb;
+////			this.status = status;
+////			this.potrebnoPovabilo = potrebnoPovabilo;
+////		}
+//
+//		public String getNaziv() {
+//			return naziv;
+//		}
+//
+//		public void setNaziv(String naziv) {
 //			this.naziv = naziv;
+//		}
+//
+//		public String getOpis() {
+//			return opis;
+//		}
+//
+//		public void setOpis(String opis) {
 //			this.opis = opis;
+//		}
+//
+//		public String getLokacija() {
+//			return lokacija;
+//		}
+//
+//		public void setLokacija(String lokacija) {
 //			this.lokacija = lokacija;
+//		}
+//
+//		public Date getZacetek() {
+//			return zacetek;
+//		}
+//
+//		public void setZacetek(Date zacetek) {
 //			this.zacetek = zacetek;
+//		}
+//
+//		public Date getTrajanje() {
+//			return trajanje;
+//		}
+//
+//		public void setTrajanje(Date trajanje) {
 //			this.trajanje = trajanje;
+//		}
+//
+//		public Date getKonecPrijav() {
+//			return konecPrijav;
+//		}
+//
+//		public void setKonecPrijav(Date konecPrijav) {
 //			this.konecPrijav = konecPrijav;
+//		}
+//
+//		public Date getKonecOdjav() {
+//			return konecOdjav;
+//		}
+//
+//		public void setKonecOdjav(Date konecOdjav) {
 //			this.konecOdjav = konecOdjav;
+//		}
+//
+//		public int getMinOseb() {
+//			return minOseb;
+//		}
+//
+//		public void setMinOseb(int minOseb) {
 //			this.minOseb = minOseb;
+//		}
+//
+//		public int getMaxOseb() {
+//			return maxOseb;
+//		}
+//
+//		public void setMaxOseb(int maxOseb) {
 //			this.maxOseb = maxOseb;
+//		}
+//
+//		public String getStatus() {
+//			return status;
+//		}
+//
+//		public void setStatus(String status) {
 //			this.status = status;
+//		}
+//
+//		public boolean isPotrebnoPovabilo() {
+//			return potrebnoPovabilo;
+//		}
+//
+//		public void setPotrebnoPovabilo(boolean potrebnoPovabilo) {
 //			this.potrebnoPovabilo = potrebnoPovabilo;
 //		}
-
-		public String getNaziv() {
-			return naziv;
-		}
-
-		public void setNaziv(String naziv) {
-			this.naziv = naziv;
-		}
-
-		public String getOpis() {
-			return opis;
-		}
-
-		public void setOpis(String opis) {
-			this.opis = opis;
-		}
-
-		public String getLokacija() {
-			return lokacija;
-		}
-
-		public void setLokacija(String lokacija) {
-			this.lokacija = lokacija;
-		}
-
-		public Date getZacetek() {
-			return zacetek;
-		}
-
-		public void setZacetek(Date zacetek) {
-			this.zacetek = zacetek;
-		}
-
-		public Date getTrajanje() {
-			return trajanje;
-		}
-
-		public void setTrajanje(Date trajanje) {
-			this.trajanje = trajanje;
-		}
-
-		public Date getKonecPrijav() {
-			return konecPrijav;
-		}
-
-		public void setKonecPrijav(Date konecPrijav) {
-			this.konecPrijav = konecPrijav;
-		}
-
-		public Date getKonecOdjav() {
-			return konecOdjav;
-		}
-
-		public void setKonecOdjav(Date konecOdjav) {
-			this.konecOdjav = konecOdjav;
-		}
-
-		public int getMinOseb() {
-			return minOseb;
-		}
-
-		public void setMinOseb(int minOseb) {
-			this.minOseb = minOseb;
-		}
-
-		public int getMaxOseb() {
-			return maxOseb;
-		}
-
-		public void setMaxOseb(int maxOseb) {
-			this.maxOseb = maxOseb;
-		}
-
-		public String getStatus() {
-			return status;
-		}
-
-		public void setStatus(String status) {
-			this.status = status;
-		}
-
-		public boolean isPotrebnoPovabilo() {
-			return potrebnoPovabilo;
-		}
-
-		public void setPotrebnoPovabilo(boolean potrebnoPovabilo) {
-			this.potrebnoPovabilo = potrebnoPovabilo;
-		}
-
-			
-	}
-
-
-
-	
-
+//
+//			
+//	}
+//
+//
+//
+//	
+//
