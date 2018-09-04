5481189398e6aff38ebdee41ddac707d00ea6d56
Master <Master@DESKTOP-0SP7H74>
2018-09-04 23:12:37 +0200

    a

--- PraktikumDogodki/src/main/java/com/praktikum/dogodki/repositorij/DogodekRepositorij.java
+++ PraktikumDogodki/src/main/java/com/praktikum/dogodki/repositorij/DogodekRepositorij.java
@@ -1,14 +1,14 @@
-package com.praktikum.dogodki.repositorij;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.praktikum.dogodki.baza.Dogodki;
-
-/**
- * Created by jt on 1/10/17.
- */
-public interface DogodekRepositorij extends CrudRepository<Dogodki, Long> {
-
-	Iterable<Dogodki> findAll();
-
-}
+//package com.praktikum.dogodki.repositorij;
+//
+//import org.springframework.data.repository.CrudRepository;
+//
+//import com.praktikum.dogodki.baza.Dogodki;
+//
+///**
+// * Created by jt on 1/10/17.
+// */
+//public interface DogodekRepositorij extends CrudRepository<Dogodki, Long> {
+//
+//	Iterable<Dogodki> findAll();
+//
+//}
