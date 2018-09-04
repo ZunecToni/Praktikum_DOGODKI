f381ae18cefbed18b0b117d54b654c07585573ab
Master <Master@DESKTOP-0SP7H74>
2018-09-04 23:21:06 +0200

    d

--- PraktikumDogodki/src/main/java/com/praktikum/dogodki/repositorij/OrganizatorRepozitorij.java
+++
@@ -1,7 +0,0 @@
-package com.praktikum.dogodki.repositorij;
-import org.springframework.data.repository.CrudRepository;
-import com.praktikum.dogodki.baza.Organizatorji;
-
-public interface OrganizatorRepozitorij extends CrudRepository<Organizatorji, Long> {
-
-}
